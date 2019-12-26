b20b5a30d52464526bcc058fb99d6c10d2040325
thegigibeast <thegigibeast@gmail.com>
2019-12-26 09:48:26 -0500

    Crimslime Fix

---
+++ AnnoyingModConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace AnnoyingMod
+{
+    [Label("Configuration")]
+    public class AnnoyingModConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [Header("Crimson Worlds")]
+
+        [DefaultValue(true)]
+        [Label("Crimslime Fix")]
+        [Tooltip("Crimslimes will have a chance to inflict Darkness and drop the Blindfold")]
+        public bool CrimslimeFix;
+    }
+}
---
+++ NPCs/AnnoyingModGlobalNPC.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AnnoyingMod.NPCs
+{
+    public class AnnoyingModGlobalNPC : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            switch (npc.type)
+            {
+                case NPCID.BigCrimslime:
+                case NPCID.Crimslime:
+                case NPCID.LittleCrimslime:
+                    if (ModContent.GetInstance<AnnoyingModConfig>().CrimslimeFix && Main.rand.Next(Main.expertMode ? 50 : 100) == 0)
+                    {
+                        Item.NewItem(npc.getRect(), ItemID.Blindfold);
+                    }
+                    break;
+            }
+
+        }
+
+        public override void OnHitPlayer(NPC npc, Player target, int damage, bool crit)
+        {
+            switch (npc.type)
+            {
+                case NPCID.BigCrimslime:
+                case NPCID.Crimslime:
+                case NPCID.LittleCrimslime:
+                    if (ModContent.GetInstance<AnnoyingModConfig>().CrimslimeFix && Main.rand.Next(4) == 0)
+                    {
+                        target.AddBuff(BuffID.Darkness, Main.expertMode ? 1800 : 900);
+                    }
+                    break;
+            }
+        }
+    }
+}
